ecf26adf56d6e0390ae6695ad1c87053c98e48c0
Claire_Sullivan <clairesullivan76@gmail.com>
2025-03-18 21:52:30 -0400

    added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/clairesull/m09_inclass',
+    author='CS',
+    author_email='kyqrs@virginia.edm',
+    license='MIT',
+    packages=['package_demo'])
